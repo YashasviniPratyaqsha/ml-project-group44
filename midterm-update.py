fae0cdcdac0cc4e90a82aa8d804de912d671c4c5
YashasviniPratyaqsha <40126312+YashasviniPratyaqsha@users.noreply.github.com>
2024-11-08 20:49:25 -0500

    Create midterm-update.py
    Creating Streamlit app for midterm update

---
+++ midterm-update.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.markdown("""
+    <style>
+    .justified-text {
+        text-align: justify;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.title('Project Midterm Update')
